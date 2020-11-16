c3ba4fa2a9aa98c95aad6eafa522c8626e64070f
Łukasz Stępka <lukaszpinoos@gmail.com>
2020-11-16 18:42:06 +0100

    class js__injection and add innerHTML

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const name = "Łukasz";
 const age = "29";
 
 console.log(`Cześć jestem ${name} i mam ${age} lat`);
+const header = document.querySelector('.js__injection');
+header.innerHTML = '<span style="color:red; font-size:500%;"><b>Poznaj moc JS-a !!!</b></span>';
+console.log(header);
