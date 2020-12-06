406f50a3fc0426d8b5088e3fc374bb1f3c766690
Lukasz Stepka <lukaszpinoos@gmail.com>
2020-12-06 12:15:59 +0100

    complite hamburger menu

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const age = "29";
 
 console.log(`Cześć jestem ${name} i mam ${age} lat`);
 const header = document.querySelector('.js__injection');
-header.innerHTML = '<span style="color:red; font-size:500%;"><b>Poznaj moc JS-a !!!</b></span>';
+//header.innerHTML = '<span style="color:red; font-size:500%;"><b>Poznaj moc JS-a !!!</b></span>';
 console.log(header);
 
 function greet(name, age) {
     const header__js =document.querySelector(`.header__js`);
-    header__js.innerHTML = `Dzień dobry ${name}! masz ${age} lat, witaj na mojej stronie;)`;
+   // header__js.innerHTML = `Dzień dobry ${name}! masz ${age} lat, witaj na mojej stronie;)`;
 }
 greet(name, age);
 
@@ -24,7 +24,7 @@ const deathStar = {
     population: 10000,
 }
 const obj = document.querySelector('.obj');
-obj.innerHTML = `Dzień najlepszy bo obiektowy= ${deathStar.name}`;
+//obj.innerHTML = `Dzień najlepszy bo obiektowy= ${deathStar.name}`;
 
 console.log(typeof "2");
 if (2 == "2") {
@@ -38,3 +38,17 @@ const humanTwo=29;
 if(humanOne.age < humanTwo){
     console.log('Jestem starszy mam 29');
 }
+
+const button = document.querySelector('.button--in--html');
+ console.log(button);
+const click = () =>{
+    const change = document.querySelector(".navigation--item");
+    change.classList.toggle('klasa--z--js');
+}
+button.addEventListener('click', click);
+  
+
+    
+    //hamburger.innerHTML = 'Udało się';
+   // hamburger.classList.toggle('.klasa--z--js');
+//})
