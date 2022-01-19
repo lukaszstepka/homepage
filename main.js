3bf9abbc16fb09db414903998f9ee7a2eed037a1
Łukasz Stępka <lukaszpinoos@gmail.com>
2022-01-19 18:03:35 +0100

    js injection complite

---
+++ main.js
@@ -0,0 +1,8 @@
+const hello = "siema siema wszystkim";
+console.log(hello);
+
+const information = 'Ten napis został dodany za pomocą JavaScript';
+
+const adding = document.querySelector('.js--added');
+
+adding.innerHTML = `${hello} ---> ${information}`;
