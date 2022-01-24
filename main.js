cd2add501a41933dd702d8c406abf3c090be3344
Łukasz Stępka <lukaszpinoos@gmail.com>
2022-01-24 21:56:56 +0100

    adding hello js code  to JS website

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const hello = "siema siema wszystkim";
+const hello = "Cześć wszystkim mam na imię ";
 console.log(hello);
+const my_name = "Łukasz"; 
+const age = 30; 
 
-const information = 'Ten napis został dodany za pomocą JavaScript';
+let hello_my_friend = (my_name,age);
 
 const adding = document.querySelector('.article__day--jsContent');
 
-adding.innerHTML = `${hello} ---> ${information}`;
+adding.innerHTML = `${hello} ${my_name}  i mam  ${age} lat.`;
